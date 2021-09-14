57c538177f1902a9f844f8d7b13e25c7db0363f7
hoanghg45 <85442015+hoanghg45@users.noreply.github.com>
2021-09-14 21:08:33 +0700

    Update

---
+++ app/src/main/java/com/example/appapi/Tc.java
@@ -0,0 +1,14 @@
+package com.example.appapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tc);
+    }
+}
